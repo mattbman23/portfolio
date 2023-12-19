0ef998a12b058814f5c1a548db5d11f70eb0c610
Matthew Budiman <matthewbudiman@outlook.com>
2023-12-19 12:31:31 -0600

    Fix type error

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -4,7 +4,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+  if (req.query.secret !== process.env.REVALIDATE_TOKEN!) {
     return res.status(401).json({ message: "Invalid Token" });
   }
 
